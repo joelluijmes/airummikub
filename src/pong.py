a7d378c4dfa968ddf00b39b7bd0c0f57750b441b
Joël Luijmes <me@joell.dev>
2024-05-09 23:15:35 +0200

    refactor: into Game

--- src/pong.py
+++ src/pong.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import random
 
 
 SIZE = WIDTH, HEIGHT = 800, 600
@@ -13,7 +14,7 @@ RED = 255, 0, 0
 
 
 class Paddle(pygame.sprite.Sprite):
-    SPEED = 5
+    SPEED = 8
 
     def __init__(self, color, width, height, left_position):
         super().__init__()
@@ -57,79 +58,69 @@ class Ball(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-        self.speed = [5, 5]
+        # self.speed = random.choice([[5, 5], [-5, 5], [5, -5], [-5, -5]])
+        self.speed = [0, 0]
 
     def update(self):
         self.rect = self.rect.move(self.speed)
 
 
+class Game:
+    def __init__(self, framerate=60):
+        self.screen = pygame.display.set_mode(SIZE)
+        self.clock = pygame.time.Clock()
+
+        self.paddle = Paddle(color=WHITE, width=10, height=50, left_position=PADDLE_OFFSET)
+        self.ball = Ball(color=WHITE, radius=10, x=WIDTH // 2, y=HEIGHT // 2)
+        self.wall_top = Wall(color=WHITE, width=WIDTH, height=WALL_WIDTH, x=0, y=0)
+        self.wall_bottom = Wall(color=WHITE, width=WIDTH, height=WALL_WIDTH, x=0, y=HEIGHT - 10)
+        self.wall_right = Wall(color=WHITE, width=WALL_WIDTH, height=HEIGHT, x=WIDTH - WALL_WIDTH, y=0)
+
+        self.sprites = pygame.sprite.Group(
+            self.paddle,
+            self.ball,
+            self.wall_top,
+            self.wall_bottom,
+            self.wall_right,
+        )
+
+        self.framerate = framerate
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Check for game over
+            if self.ball.rect.left < 0:
+                self.screen.fill(color=RED)
+                pygame.display.flip()
+                self.clock.tick(60)
+                continue
+
+            # Check for collisions
+            if pygame.sprite.spritecollide(self.ball, [self.wall_right], False):
+                self.ball.speed[0] *= -1
+            if pygame.sprite.spritecollide(self.ball, [self.wall_top, self.wall_bottom], False):
+                self.ball.speed[1] *= -1
+            if pygame.sprite.spritecollide(self.ball, [self.paddle], False):
+                self.ball.speed[0] *= -1
+
+            # Redraw the screen
+            self.sprites.update()
+            self.screen.fill(color=BLACK)
+            self.sprites.draw(self.screen)
+
+            pygame.display.flip()
+            self.clock.tick(self.framerate)
+
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(SIZE)
 
-    sprites = pygame.sprite.Group()
-    paddle = Paddle(color=WHITE, width=10, height=50, left_position=PADDLE_OFFSET)
-    sprites.add(paddle)
-    ball = Ball(color=WHITE, radius=10, x=WIDTH // 2, y=HEIGHT // 2)
-    sprites.add(ball)
-
-    wall_top = Wall(color=WHITE, width=WIDTH, height=WALL_WIDTH, x=0, y=0)
-    wall_bottom = Wall(color=WHITE, width=WIDTH, height=WALL_WIDTH, x=0, y=HEIGHT - 10)
-    wall_right = Wall(color=WHITE, width=WALL_WIDTH, height=HEIGHT, x=WIDTH - WALL_WIDTH, y=0)
-
-    sprites.add(wall_top)
-    sprites.add(wall_bottom)
-    sprites.add(wall_right)
-
-    clock = pygame.time.Clock()
-
-    # Enter the game loop
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Check for game over
-        if ball.rect.left < 0 or ball.rect.right > WIDTH:
-            screen.fill(color=RED)
-            pygame.display.flip()
-            clock.tick(60)
-            continue
-
-        # Process movements
-        sprites.update()
-
-        # Draw everything
-        screen.fill(color=BLACK)
-        sprites.draw(screen)
-
-        # Check wall collisions, they are just perpendicular to the ball's speed
-        if pygame.sprite.spritecollide(ball, [wall_right], False):
-            ball.speed[0] *= -1
-        if pygame.sprite.spritecollide(ball, [wall_top, wall_bottom], False):
-            ball.speed[1] *= -1
-
-        # Check paddle collision, here we check where the ball is hitting the paddle
-        if pygame.sprite.spritecollide(ball, [paddle], False):
-            # This doesn't seem to work, lets go with basic version
-            ball.speed[0] *= -1
-
-            # intersection_point = pygame.sprite.collide_mask(ball, paddle)
-            # if intersection_point:
-            #     relative_intersect_y = (paddle.rect.y + paddle.rect.height // 2) - intersection_point[1]
-            #     normalized_intersect_y = relative_intersect_y / (paddle.rect.height // 2)
-
-            #     # Calculate the bounce angle based on the collision position
-            #     bounce_angle = normalized_intersect_y * math.radians(75)
-
-            #     # Calculate the new velocity components
-            #     speed = 5
-            #     ball.speed[0] = -speed * math.cos(bounce_angle)
-            #     ball.speed[1] = speed * math.sin(bounce_angle)
-
-        # Update the display
-        pygame.display.flip()
-        clock.tick(60)
+    game = Game()
+    game.run()
 
 
 if __name__ == "__main__":
